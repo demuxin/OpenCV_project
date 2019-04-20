9dd06fc2accdd14006306d5a8491ea7f61f81dd0
xiaode <xiaode2314@163.com>
2019-04-20 20:55:37 +0800

    distance to camera

---
+++ distance_to_camera/distance_to_camera.py
@@ -0,0 +1,67 @@
+from imutils import paths
+import numpy as np
+import imutils
+import cv2
+
+# initialize the known distance from the camera to the object, which
+# in this case is 24 inches
+KNOWN_DISTANCE = 24.0
+
+# initialize the known object width, which in this case, the piece of
+# paper is 12 inches wide
+KNOWN_WIDTH = 11.0
+
+def get_focalLength():
+    # load the furst image that contains an object that is KNOWN TO BE 2 feet
+    # from our camera, then find the paper marker in the image, and initialize
+    # the focal length
+    image = cv2.imread("./2ft.jpg")
+    marker = find_marker(image)
+    focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+
+    return focalLength
+
+
+def find_marker(image):
+    # convert the image to grayscale, blur it, and detect edges
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    edged = cv2.Canny(gray, 35, 125)
+
+    # find the contours in the edged image and keep the largest one;
+    # we'll assume that this is our piece of paper in the image
+    cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = imutils.grab_contours(cnts)
+    c = max(cnts, key = cv2.contourArea)
+
+    # compute the bounding box of the of the paper region and return it
+    return cv2.minAreaRect(c)
+
+
+def distance_to_camera(knownWidth, focalLength, perWidth):
+    # compute and return the distance from the maker to the camera
+    return (knownWidth * focalLength) / perWidth
+
+
+if __name__ == "__main__":
+
+    focalLength = get_focalLength()
+
+    # loop over the images
+    for imagePath in sorted(paths.list_images("images")):
+        # load the image, find the marker in the image, then compute the
+        # distance to the marker from the camera
+        image = cv2.imread(imagePath)
+        marker = find_marker(image)
+        inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
+
+        # draw a bounding box around the image and display it
+        box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
+        box = np.int0(box)
+        cv2.drawContours(image, [box], -1, (0, 255, 0), 2)
+        cv2.putText(image, "%.2fft" % (inches / 12),
+            (image.shape[1] - 200, image.shape[0] - 20), cv2.FONT_HERSHEY_SIMPLEX,
+            2.0, (0, 255, 0), 3)
+        cv2.imshow("image", image)
+
+        cv2.waitKey(0)
